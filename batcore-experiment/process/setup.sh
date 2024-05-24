0f9260cbfe410437ad18817323160f2502738b51
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2024-05-25 00:06:33 +0330

    better batc setup

--- batcore-experiment/process/setup.sh
+++ batcore-experiment/process/setup.sh
@@ -5,8 +5,16 @@ REPO_URL="https://github.com/JetBrains-Research/batcore.git"
 DIR_NAME="batcore-repo"
 
 if [ ! -d "$DIR_NAME" ]; then
-    echo "Directory $DIR_NAME does not exist. Cloning repository..."
-    git clone --branch main --depth 1 $REPO_URL $DIR_NAME
+  echo "Directory $DIR_NAME does not exist. Cloning repository..."
+  git clone --branch main --depth 1 $REPO_URL $DIR_NAME
 else
-    echo "Directory $DIR_NAME already exists."
+  echo "Directory $DIR_NAME already exists."
 fi
+
+pacman -S tk
+# install pyenv and pyenv-virtualenv
+pyenv install 3.8.19
+pyenv virtualenv 3.8 batc
+pyenv shell batc
+#pyenv activate batc
+uv pip install -r ./requirements.txt
