835e6e3893dbbcc6bc28a2ea46a690b077b543b0
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2024-07-19 18:13:23 +0330

    remove other impls

--- others-impls/webstorm/rm-extention/src/main/java/Metrics.java
+++
@@ -1,8 +0,0 @@
-import com.rapidminer.example.ExampleSet;
-import ir.msdehghan.rm.RapidMinerCalculator;
-
-public class Metrics {
-    public static ExampleSet process(ExampleSet input) {
-        return RapidMinerCalculator.processMetrics(input);
-    }
-}
